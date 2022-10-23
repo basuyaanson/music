0affa2d21e3a9a490c5b8c71bae4705a10eed6c4
basuyaanson <115710096+basuyaanson@users.noreply.github.com>
2022-10-24 00:51:36 +0800

    Add files via upload

---
+++ common.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <map> 
+#include<ctime>
+#include<cstdlib>
+#include<filesystem>
+#include <vector>
+#include <cstdio>
+#include<Windows.h>
+#include <stdlib.h>
+#include <mmsystem.h>
+#pragma comment(lib,"winmm.lib") //ÀRºA®w
+using namespace std;
